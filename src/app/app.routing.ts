90dc3c7068b0e97d7a8085b4c0b3819e03c8e458
ddrajurkar <ddrajurkar@gmail.com>
2018-05-21 17:30:54 +0530

    routing completed
    routing completed

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,15 @@ import { ModuleWithProviders } from '@angular/core/src/metadata/ng_module';
 import { CalcComponentComponent } from './calc-component/calc-component.component';
 import { WordComponent } from './word/word.component';
 import { GetDataComponent } from './get-data/get-data.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
 
 export const AppRoutes: Routes = [
+    { path: '',component:HomeComponent},
     { path: 'calc', component: CalcComponentComponent },
     { path: 'word',component:WordComponent },
-    { path: 'getData',component:GetDataComponent}
+    { path: 'getData',component:GetDataComponent},
+    { path: '**',component: NotfoundComponent}
 ];
 export const ROUTING: ModuleWithProviders = RouterModule.forRoot(AppRoutes);
